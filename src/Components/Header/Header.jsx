1f29c24275bcd2a10350e0d41dc704d5fa9def6e
AbdullahWB <abdullah917828@gmail.com>
2023-04-07 16:46:21 +0600

    add banner and this content

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <>
-            <div className="navbar bg-base-100 lg:mt-12 md:mt-11">
-                <div className="navbar-start">
+            <div className="navbar bg-base-100 lg:mt-12 md:mt-11 mb-10">
+                <div className="navbar-start lg:ml-[150px]">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -26,23 +26,7 @@ const Header = () => {
                     </div>
                     <a className="btn btn-ghost normal-case text-2xl font-extrabold ">Tasty HUT</a>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a>
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end lg:mr-10 md:mr-10">
+                <div className="navbar-end lg:mr-10 md:mr-10 lg:mr-[150px]">
                     <a className="btn btn-primary text-black text-xl font-bold">Sign Up</a>
                 </div>
             </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,22 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <div className='w-full h-[650px]'>
+            <div className='w-full h-[650px] relative'>
                 <img className='object-cover w-full h-auto lg:h-full' src="Rectangle3978.png" alt="Rectangle3978.png" />
+                <div className='w-full h-full absolute top-0 left-0 bg-black bg-opacity-70 text-center flex justify-center items-center flex-col'>
+                    <h1 className='lg:text-6xl text-white md:text-4xl text-2xl font-extrabold mb-6'>Taste Our Delicious <br /> Best Foods</h1>
+                    <p className='text-[18px] text-white mb-6'>There are many variations of passages of Lorem Ipsum available, but the majority have <br /> suffered alteration in some form, by injected humou.</p>
+                    <form>
+                        <div className="form-control">
+                            <div className="input-group justify-center">
+                                <input type="text" placeholder="Search…" className="input input-bordered py-4 pr-[400px]" />
+                                <button className="btn btn-primary px-9 py-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 pb-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
